da5dd3bc70fece2da812e2e2de6d7ee93a2d2b73
tangchuan <tangchuanjiaoshou@gmail.com>
2023-06-02 00:32:56 +0800

    navigate between pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="/posts/first-post">this page!</a>
         </h1>
 
         <p className={styles.description}>
